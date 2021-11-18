fb6c75238274baffbe261138f0d2077a36e525c5
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-11-18 03:13:05 -0300

    Desafio23. 🚀

---
+++ challenges/desafio23.js
@@ -0,0 +1,12 @@
+db.voos.findOne(
+  { $and: [
+    { litrosCombustivel: { $lte: 1000 } },
+    { litrosCombustivel: { $exists: true } },
+  ],
+  },
+  {
+    _id: false,
+    vooId: true,
+    litrosCombustivel: true,
+  },
+);
