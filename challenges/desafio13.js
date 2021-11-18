7fa17591b6adc000393917b89a048e3328686583
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-11-18 01:42:59 -0300

    Desafios12 e 13.

---
+++ challenges/desafio13.js
@@ -0,0 +1,8 @@
+db.voos
+  .count({
+    $nor: [
+      { "aeroportoDestino.continente": { $eq: "EUROPA" } },
+      { "aeroportoDestino.continente": { $eq: "ÁSIA" } },
+      { "aeroportoDestino.continente": { $eq: "OCEANIA" } },
+    ],
+  });
